4e3753ab3bb10b3c482c62de7216be98f67fb675
Yang Liu <50459973+ly4096x@users.noreply.github.com>
2020-07-26 11:08:38 -0400

    add footer; modify generate script to allow LastPage be set

--- GenerateAll.sh
+++ GenerateAll.sh
@@ -1,7 +1,9 @@
 mkdir -p output
+for i in {1..3}; do
 pdflatex -output-directory=output -jobname='Resume_Software_OnePage' '\def\resVersion{Software} \def\resPresetFormat{OnePage} \input{ResumeContent.tex}'
 pdflatex -output-directory=output -jobname='Resume_Software_TwoPage' '\def\resVersion{Software} \def\resPresetFormat{TwoPage} \input{ResumeContent.tex}'
 pdflatex -output-directory=output -jobname='Resume_GeneralPurpose_OnePage' '\def\resVersion{GeneralPurpose} \def\resPresetFormat{OnePage} \input{ResumeContent.tex}'
 pdflatex -output-directory=output -jobname='Resume_GeneralPurpose_TwoPage' '\def\resVersion{GeneralPurpose} \def\resPresetFormat{TwoPage} \input{ResumeContent.tex}'
+done
 cd output
 rm *.out *.aux *.log
