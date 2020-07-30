b21035bd5646e8b6f036578247dff18790715a53
Yang Liu <50459973+ly4096x@users.noreply.github.com>
2020-07-30 00:01:15 -0400

    add email field in Config.tex; change default Config.tex

--- GenerateAll.sh
+++ GenerateAll.sh
@@ -1,9 +1,11 @@
+EMAIL='50459973+ly4096x@users.noreply.github.com'
+
 mkdir -p output
-for i in {1..3}; do
-pdflatex -output-directory=output -jobname='Resume_Software_OnePage' '\def\resVersion{Software} \def\resPresetFormat{OnePage} \input{ResumeContent.tex}'
-pdflatex -output-directory=output -jobname='Resume_Software_TwoPage' '\def\resVersion{Software} \def\resPresetFormat{TwoPage} \input{ResumeContent.tex}'
-pdflatex -output-directory=output -jobname='Resume_GeneralPurpose_OnePage' '\def\resVersion{GeneralPurpose} \def\resPresetFormat{OnePage} \input{ResumeContent.tex}'
-pdflatex -output-directory=output -jobname='Resume_GeneralPurpose_TwoPage' '\def\resVersion{GeneralPurpose} \def\resPresetFormat{TwoPage} \input{ResumeContent.tex}'
+for i in {1..2}; do
+pdflatex -output-directory=output -jobname='Resume_Software_OnePage'       '\def\resMyEmail{'"$EMAIL"'} \def\resVersion{Software}       \def\resPresetFormat{OnePage} \input{ResumeContent.tex}'
+pdflatex -output-directory=output -jobname='Resume_Software_TwoPage'       '\def\resMyEmail{'"$EMAIL"'} \def\resVersion{Software}       \def\resPresetFormat{TwoPage} \input{ResumeContent.tex}'
+pdflatex -output-directory=output -jobname='Resume_GeneralPurpose_OnePage' '\def\resMyEmail{'"$EMAIL"'} \def\resVersion{GeneralPurpose} \def\resPresetFormat{OnePage} \input{ResumeContent.tex}'
+pdflatex -output-directory=output -jobname='Resume_GeneralPurpose_TwoPage' '\def\resMyEmail{'"$EMAIL"'} \def\resVersion{GeneralPurpose} \def\resPresetFormat{TwoPage} \input{ResumeContent.tex}'
 done
 cd output
 rm *.out *.aux *.log
