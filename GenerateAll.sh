9bc128e7996368c291b830ffaeee601aed8115c9
Yang Liu <50459973+ly4096x@users.noreply.github.com>
2020-08-14 04:34:27 -0400

    hide awards in OnePage; add contact info

--- GenerateAll.sh
+++ GenerateAll.sh
@@ -1,11 +1,15 @@
 EMAIL='50459973+ly4096x@users.noreply.github.com'
+PHONE='+1-(000)-000-0000'
+ADDR='100 Anonymous St, New York NY 10001'
+
+DEFAULT_INFO="\\def\\resMyEmail{$EMAIL} \\def\\resMyPhone{$PHONE} \\def\\resMyAddress{$ADDR}"
 
 mkdir -p output
 for i in {1..2}; do
-pdflatex -output-directory=output -jobname='Resume_Software_OnePage'       '\def\resMyEmail{'"$EMAIL"'} \def\resVersion{Software}       \def\resPresetFormat{OnePage} \input{ResumeContent.tex}'
-pdflatex -output-directory=output -jobname='Resume_Software_TwoPage'       '\def\resMyEmail{'"$EMAIL"'} \def\resVersion{Software}       \def\resPresetFormat{TwoPage} \input{ResumeContent.tex}'
-pdflatex -output-directory=output -jobname='Resume_GeneralPurpose_OnePage' '\def\resMyEmail{'"$EMAIL"'} \def\resVersion{GeneralPurpose} \def\resPresetFormat{OnePage} \input{ResumeContent.tex}'
-pdflatex -output-directory=output -jobname='Resume_GeneralPurpose_TwoPage' '\def\resMyEmail{'"$EMAIL"'} \def\resVersion{GeneralPurpose} \def\resPresetFormat{TwoPage} \input{ResumeContent.tex}'
+pdflatex -output-directory=output -jobname='Resume_Software_OnePage'       "$DEFAULT_INFO"' \def\resVersion{Software}       \def\resPresetFormat{OnePage} \input{ResumeContent.tex}'
+pdflatex -output-directory=output -jobname='Resume_Software_TwoPage'       "$DEFAULT_INFO"' \def\resVersion{Software}       \def\resPresetFormat{TwoPage} \input{ResumeContent.tex}'
+pdflatex -output-directory=output -jobname='Resume_GeneralPurpose_OnePage' "$DEFAULT_INFO"' \def\resVersion{GeneralPurpose} \def\resPresetFormat{OnePage} \input{ResumeContent.tex}'
+pdflatex -output-directory=output -jobname='Resume_GeneralPurpose_TwoPage' "$DEFAULT_INFO"' \def\resVersion{GeneralPurpose} \def\resPresetFormat{TwoPage} \input{ResumeContent.tex}'
 done
 cd output
 rm *.out *.aux *.log
