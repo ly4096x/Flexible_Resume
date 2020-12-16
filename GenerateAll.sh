c08052d58edbc018a590de0bc77040ecab195207
Yang Liu <50459973+ly4096x@users.noreply.github.com>
2020-12-15 23:48:58 -0500

    minor correction. add notforwork preset

--- GenerateAll.sh
+++ GenerateAll.sh
@@ -6,10 +6,11 @@ DEFAULT_INFO="\\def\\resMyEmail{$EMAIL} \\def\\resMyPhone{$PHONE} \\def\\resMyAd
 
 mkdir -p output
 for i in {1..2}; do
-pdflatex -output-directory=output -jobname='Resume_Software_OnePage'        "$DEFAULT_INFO"' \def\resVersion{Software}       \def\resPresetFormat{OnePage} \input{ResumeContent.tex}'
-pdflatex -output-directory=output -jobname='Resume_Software_TwoPage'        "$DEFAULT_INFO"' \def\resVersion{Software}       \def\resPresetFormat{TwoPage} \input{ResumeContent.tex}'
-pdflatex -output-directory=output -jobname='Resume_SoftAndHardware_OnePage' "$DEFAULT_INFO"' \def\resVersion{SoftAndHardware} \def\resPresetFormat{OnePage} \input{ResumeContent.tex}'
-pdflatex -output-directory=output -jobname='Resume_SoftAndHardware_TwoPage' "$DEFAULT_INFO"' \def\resVersion{SoftAndHardware} \def\resPresetFormat{TwoPage} \input{ResumeContent.tex}'
+pdflatex -output-directory=output -jobname='Resume_Software_OnePage'           "$DEFAULT_INFO"' \def\resVersion{Software}        \def\resPresetFormat{OnePage}    \input{ResumeContent.tex}'
+pdflatex -output-directory=output -jobname='Resume_Software_TwoPage'           "$DEFAULT_INFO"' \def\resVersion{Software}        \def\resPresetFormat{TwoPage}    \input{ResumeContent.tex}'
+pdflatex -output-directory=output -jobname='Resume_SoftAndHardware_OnePage'    "$DEFAULT_INFO"' \def\resVersion{SoftAndHardware} \def\resPresetFormat{OnePage}    \input{ResumeContent.tex}'
+pdflatex -output-directory=output -jobname='Resume_SoftAndHardware_TwoPage'    "$DEFAULT_INFO"' \def\resVersion{SoftAndHardware} \def\resPresetFormat{TwoPage}    \input{ResumeContent.tex}'
+pdflatex -output-directory=output -jobname='Resume_SoftAndHardware_NotForWork' "$DEFAULT_INFO"' \def\resVersion{SoftAndHardware} \def\resPresetFormat{NotForWork} \input{ResumeContent.tex}'
 done
 cd output
 rm *.out *.aux *.log
